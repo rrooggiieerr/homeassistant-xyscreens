b05945a6875c1d0a838fcdb2d4d09b0ec4086f5f
Rogier van Staveren <rogier@batoid.com>
2026-02-20 14:31:50 +0100

    Update signature of platforms' async_setup_entry
    As per https://github.com/home-assistant/core/pull/138201

--- custom_components/xyscreens/cover.py
+++ custom_components/xyscreens/cover.py
@@ -15,7 +15,7 @@ from homeassistant.components.cover import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.restore_state import RestoreEntity
 from xyscreens import XYScreens, XYScreensState
 
@@ -37,7 +37,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+    async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up the XY Screens cover."""
     async_add_entities(
